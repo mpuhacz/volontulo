fb0e6c21108b4252329438fb6b86225d41693eac
mpuhacz <marcinpuhacz@gmail.com>
2016-02-27 02:24:14 +0100

    security fix, move to master asap

--- apps/volontulo/views/pages.py
+++ apps/volontulo/views/pages.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=missing-docstring
+from django.contrib.auth.decorators import user_passes_test
 from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import redirect
+from django.utils.decorators import method_decorator
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import (
     CreateView, UpdateView, DeleteView
@@ -16,6 +18,11 @@ from apps.volontulo.models import Page
 class PageList(ListView):
     model = Page
     template_name = 'pages/page_list.html'
+
+    @method_decorator(user_passes_test(
+            lambda u: u.is_authenticated() and u.userprofile.is_administrator))
+    def dispatch(self, *args, **kwargs):
+        return super(PageList, self).dispatch(*args, **kwargs)
 
 
 # pylint: disable=too-many-ancestors,missing-docstring
@@ -42,6 +49,11 @@ class PageCreate(CreateView):
         self.object.save()
         return redirect(self.get_success_url())
 
+    @method_decorator(user_passes_test(
+        lambda u: u.is_authenticated() and u.userprofile.is_administrator))
+    def dispatch(self, *args, **kwargs):
+        return super(PageCreate, self).dispatch(*args, **kwargs)
+
 
 # pylint: disable=too-many-ancestors,missing-docstring
 class PageEdit(UpdateView):
@@ -53,6 +65,11 @@ class PageEdit(UpdateView):
     )
     template_name = 'pages/page_edit_form.html'
     success_url = reverse_lazy('pages_list')
+
+    @method_decorator(user_passes_test(
+            lambda u: u.is_authenticated() and u.userprofile.is_administrator))
+    def dispatch(self, *args, **kwargs):
+        return super(PageEdit, self).dispatch(*args, **kwargs)
 
 
 # pylint: disable=too-many-ancestors,missing-docstring
@@ -66,3 +83,8 @@ class PageDelete(DeleteView):
         without confirmation
         """
         return self.post(request, *args, **kwargs)
+
+    @method_decorator(user_passes_test(
+        lambda u: u.userprofile.is_administrator and u.is_authenticated()))
+    def dispatch(self, *args, **kwargs):
+        return super(PageDelete, self).dispatch(*args, **kwargs)
